5d128a71aba0acd1ce80594e919be5aba4dd21d9
Mohit Paddhariya <dev.mohit2006@gmail.com>
2025-02-18 10:01:31 +0530

    fix: replace the prop name in google-analytics.jsx component

--- src/components/google-analytics.jsx
+++ src/components/google-analytics.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 
-const GoogleAnalytics = ({ googleAnalytics }) => {
+const GoogleAnalytics = ({ trackingId }) => {
   useEffect(() => {
     const script = document.createElement("script");
     script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${googleAnalytics}`
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${trackingId}`
     document.head.appendChild(script);
 
     script.onload = () => {
@@ -13,7 +13,7 @@ const GoogleAnalytics = ({ googleAnalytics }) => {
         window.dataLayer.push(arguments);
       }
       gtag("js", new Date());
-      gtag("config", googleAnalytics);
+      gtag("config", trackingId);
     };
   }, []);
 
