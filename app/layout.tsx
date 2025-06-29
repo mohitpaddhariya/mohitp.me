3b4e68e69f1677f5a6d7a6936e2777be65e4e877
Mohit Paddhariya <dev.mohit2006@gmail.com>
2025-06-29 09:24:10 +0530

    feat(initial template): added basic next with typescript project

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Mohit Paddhariya | Full-Stack & Gen-AI Developer",
+  description: "Mohit Paddhariya - FullStack & Gen-AI Developer specializing in scalable web and mobile applications, AI integrations, and open-source innovation.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
