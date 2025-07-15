467d42c42417020bfc70ef6562746e7473b1620f
Mohit Paddhariya <dev.mohit2006@gmail.com>
2025-07-16 00:54:37 +0530

    feat: updated resume - Mohit Paddhariya

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -145,7 +145,7 @@ const Page = () => {
                           </Link>
 
                           <Link
-                            href="/resume.pdf"
+                            href="/mohit_paddhariya's_resume.pdf"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="w-full border-2 border-theme-border text-theme px-6 py-4 rounded-xl font-saprona-medium 
